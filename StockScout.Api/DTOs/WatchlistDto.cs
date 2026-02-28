2e6c09f5085a861e09a52ecd825f650b86d59ac3
Dezu17 <dariuscpop@gmail.com>
2026-02-28 12:00:12 +0000

    Fixed dotnet formatting issues

--- StockScout.Api/DTOs/WatchlistDto.cs
+++ StockScout.Api/DTOs/WatchlistDto.cs
@@ -4,4 +4,4 @@ namespace StockScout.Api.DTOs;
 
 public record WatchlistItemDto(string Symbol);
 
-public record AddSymbolRequest([Required] [StringLength(10, MinimumLength = 2)] string Symbol);
+public record AddSymbolRequest([Required][StringLength(10, MinimumLength = 2)] string Symbol);
